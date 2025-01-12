acf332cb1f485643de149d12b7ca559c29cedc80
zawkindev <zawkindev@gmail.com>
2025-01-12 17:27:48 +0500

    generate project

---
+++ src/main/java/zawkin/me/obliquo/ObliquoApplication.java
@@ -0,0 +1,13 @@
+package zawkin.me.obliquo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObliquoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObliquoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zawkin/me/obliquo/ObliquoApplicationTests.java
@@ -0,0 +1,13 @@
+package zawkin.me.obliquo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObliquoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
